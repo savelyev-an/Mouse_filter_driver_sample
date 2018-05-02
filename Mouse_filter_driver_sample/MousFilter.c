3eccc5b0722e494756ec92d9ffb6097d62967c30
savelyev-an <savelyev_an@mail.ru>
2018-05-02 13:30:57 +0300

    Lesson Part2

--- Mouse_filter_driver_sample/MousFilter.c
+++ Mouse_filter_driver_sample/MousFilter.c
@@ -1,8 +1,22 @@
 #include <ntddk.h>
 
+NTSTATUS ObReferenceObjectByName(
+	IN PUNICODE_STRING ObjectName,
+	IN ULONG Attributes,
+	IN PACCESS_STATE AccessState,
+	IN ACCESS_MASK DesiredAccess,
+	IN POBJECT_TYPE ObjectType,
+	IN KPROCESSOR_MODE ACCESSMode,
+	IN PVOID ParseContext,
+	OUT PVOID *Object
+);
+
+
 typedef struct {
 	PDEVICE_OBJECT lowerKbdDevice;
 } DEVICE_EXTENSION, *PDEVICE_EXTENSION;
+
+extern POBJECT_TYPE *IoDriverObjectType;
 
 typedef struct _MOUSE_INPUT_DATA {
 	USHORT UnitId;
@@ -21,22 +35,33 @@ typedef struct _MOUSE_INPUT_DATA {
 } MOUSE_INPUT_DATA, *PMOUSE_INPUT_DATA;
 
 
-PDEVICE_OBJECT myKbdDevice = NULL;
-ULONG pendingkey = 0;
+// PDEVICE_OBJECT myKbdDevice = NULL; // TOREMOVE
+ULONG pendingkey = 0; // COUNTER FOR NOT FINISHED IRQLs
 
 VOID DriverUnload(PDRIVER_OBJECT DriverObject)
 {
 	LARGE_INTEGER interval = { 0 };
 	PDEVICE_OBJECT DeviceObject = DriverObject->DeviceObject;
 	interval.QuadPart = -10 * 1000 * 1000;
-	IoDetachDevice(((PDEVICE_EXTENSION)DeviceObject->DeviceExtension)->lowerKbdDevice);
+
+	while (DeviceObject) {
+	
+		IoDetachDevice(((PDEVICE_EXTENSION)DeviceObject->DeviceExtension)->lowerKbdDevice);
+		DeviceObject = DeviceObject->NextDevice;
+	}
+	
 	
 	while (pendingkey) {
 		KeDelayExecutionThread(KernelMode, FALSE, &interval);
 	}
 	
-	IoDeleteDevice(myKbdDevice);
-	KdPrint(("Unload KeyboardFilterDriver \r\n"));
+	DeviceObject = DriverObject->DeviceObject;
+	while (DeviceObject) {
+		IoDeleteDevice(DeviceObject);
+		DeviceObject = DeviceObject->NextDevice;
+	}
+	
+	KdPrint(("Unload MouseFilterDriver \r\n"));
 }
 
 NTSTATUS DispatchPass(PDEVICE_OBJECT DeviceObject, PIRP Irp) 
@@ -47,7 +72,7 @@ NTSTATUS DispatchPass(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 
 NTSTATUS ReadComplete(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context)
 {
-	CHAR* KeyFlag[4] = { "KeyDowm","KeyUp","E0","E1" };
+	// CHAR* KeyFlag[4] = { "KeyDowm","KeyUp","E0","E1" }; // TO REMOVE
 	PMOUSE_INPUT_DATA Keys = (PMOUSE_INPUT_DATA)Irp->AssociatedIrp.SystemBuffer;
 	int structnum = Irp->IoStatus.Information / sizeof(MOUSE_INPUT_DATA);
 	int i;
@@ -80,22 +105,45 @@ NTSTATUS DispatchRead(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 NTSTATUS MyAttachDevice(PDRIVER_OBJECT DriverObject)
 {
 	NTSTATUS status;
-	UNICODE_STRING TargetDevice = RTL_CONSTANT_STRING(L"\\Device\\PointerClass0");
-	status = IoCreateDevice(DriverObject, sizeof(DEVICE_EXTENSION), NULL, FILE_DEVICE_MOUSE, 0, FALSE, &myKbdDevice);
+	UNICODE_STRING MouseClassName = RTL_CONSTANT_STRING(L"\\Driver\\Mouclass");
+	PDRIVER_OBJECT targetDriverObject = NULL;
+	PDEVICE_OBJECT currentDeviceObject = NULL;
+	PDEVICE_OBJECT myDeviceObject = NULL;
+
+	status = ObReferenceObjectByName(&MouseClassName, OBJ_CASE_INSENSITIVE, NULL, 0, *IoDriverObjectType, KernelMode, NULL, (PVOID*)&targetDriverObject);
 	if (!NT_SUCCESS(status)) {
+		KdPrint(("ObReference  is failed \r\n"));
 		return status;
 	}
 
-	myKbdDevice->Flags |= DO_BUFFERED_IO;
-	myKbdDevice->Flags &= ~DO_DEVICE_INITIALIZING;
+	ObDereferenceObject(targetDriverObject);
 
-	RtlZeroMemory(myKbdDevice->DeviceExtension, sizeof(DEVICE_EXTENSION));
+	currentDeviceObject = targetDriverObject->DeviceObject;
 
-	status = IoAttachDevice(myKbdDevice, &TargetDevice, &((PDEVICE_EXTENSION)myKbdDevice->DeviceExtension)->lowerKbdDevice);
-	if (!NT_SUCCESS(status)) {
-		IoDeleteDevice(myKbdDevice);
-		return status;
+	while (currentDeviceObject != NULL) {
+		// TODO reWrite to solve the problem when you get an error at not the first currentDevice -> WE NEED THE LOOP FOR THE IoDeleteDevice !!!!!!!
+		status = IoCreateDevice(DriverObject, sizeof(DEVICE_EXTENSION), NULL, FILE_DEVICE_MOUSE, 0, FALSE, &myDeviceObject);
+		if (!NT_SUCCESS(status)) {
+			// do your work
+			return status;
+		}
+		
+		RtlZeroMemory(myDeviceObject->DeviceExtension, sizeof(DEVICE_EXTENSION));
+		status = IoAttachDeviceToDeviceStackSafe(myDeviceObject, currentDeviceObject,&((PDEVICE_EXTENSION)myDeviceObject->DeviceExtension)->lowerKbdDevice);
+		
+		if (!NT_SUCCESS(status)) {
+			// do your work
+			//IoDeleteDevice(myKbdDevice);
+			return status;
+		}
+
+		myDeviceObject->Flags |= DO_BUFFERED_IO;
+		myDeviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
+
+		currentDeviceObject = currentDeviceObject->NextDevice;
 	}
+
+	
 	return STATUS_SUCCESS;
 }
 
@@ -110,14 +158,15 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 	}
 
 	DriverObject->MajorFunction[IRP_MJ_READ] = DispatchRead;
+	//DriverObject->MajorFunction[IRP_MJ_PNP] =  IRP_MN_REMOVE_DEVICE OR SOMETHING ELSE UGLY - !!! SEE the Video part 2 since 25.03
 
 	status = MyAttachDevice(DriverObject);
 	if (!NT_SUCCESS(status)) {
-		KdPrint(("Attaching is faikied \r\n"));
+		KdPrint(("Attaching Mouse_Filter_Driver is failed \r\n"));
 
 	}
 	else {
-		KdPrint(("Attacning is succeeds \r\n"));
+		KdPrint(("Attacning Mouse_Filter_Driver is succeeds \r\n"));
 	}
 	return status;
 }
